f42018cf6af70f493e0f966781a85e6fff14d0ae
marwaothman <marwaothman97@yahoo.com>
2026-09-14 14:53:34 +0300

    Restore original presentation version

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "./",
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: "assets/index-0gp-ggxE.js",
-        chunkFileNames: "assets/chunk-[name].js",
-        assetFileNames: asset => asset.names?.some(name => name.endsWith(".css")) ? "assets/index-BqEKqRLk.css" : "assets/[name][extname]",
-      },
-    },
-  },
 });
